7d4723c08ed81eaf32399f7fbe11a1732b70e313
DerekPeng303 <derekpeng303@gmail.com>
2022-02-12 17:44:30 +1300

    multi choice answer should work for both formats

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import paper_scraper as ps
 
 def scrape_pdf_paper(category, subject_code, years, mark_scheme=False):
     years = [str(x) for x in years]
-    seasons = ["summer"]
-    time_zones = ["1"]
-    past_paper = ps.PDFPaper(category=category, subject_code=subject_code, year=years[0])
-    past_paper.subject = ps.subject_finder(past_paper)
-    print(past_paper.subject)
-    past_paper.paper_count = ps.scan_papers(past_paper)
-    ps.multi_choice_ans_finder()
-
+    answers = {"A":0, "B":0, "C":0, "D":0}
+    for year in years:
+        past_paper = ps.PDFPaper(category = category, subject_code = subject_code, year = year)
+        past_paper.subject = ps.subject_finder(past_paper)
+        paper_count = ps.scan_papers(past_paper)
+        for version in paper_count:
+            past_paper.season = version[0]
+            past_paper.paper = version[1]
+            past_paper.time_zone = version[2]
+            if past_paper.paper == "1":
+                answer = ps.multi_choice_ans_finder(past_paper)
+                for i in range(1,len(answer)):
+                    answers[answer[i]] += 1
+                    
+    print(answers)
 
 if __name__ == '__main__':
     years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
-    years = [2015]
-    scrape_pdf_paper(category="A%20Levels", subject_code="9702", years=[2020], mark_scheme=True)
+    years = [2020]
+    scrape_pdf_paper(category="A%20Levels", subject_code="9702", years=years, mark_scheme=True)
--- paper_scraper.py
+++ paper_scraper.py
@@ -5,16 +5,14 @@ import os
 import tabula
 
 class PDFPaper(object):
-    def __init__(self, category="", subject_code="", year="", season="", time_zone="", paper="", mark_scheme=False):
+    def __init__(self, category="", subject_code="", year="", season="", time_zone="", paper=""):
         self.category = category  # "A Levels", "Cambridge IGCSE"
-        self.subject_code = subject_code
         self.subject = ""
+        self.subject_code = subject_code
         self.year = year
         self.season = season  # "summer", "winter"
-        self.time_zone = time_zone
         self.paper = paper
-        self.mark_scheme = mark_scheme
-        self.paper_count = ""
+        self.time_zone = time_zone
 
 def subject_finder(pdf):
     session = requests.Session()
@@ -63,39 +61,61 @@ def scan_papers(pdf):
     paper_count = list(paper_count)
     return paper_count
 
-def multi_choice_ans_finder():
-    file = open('test.pdf', 'rb')
-    pdf = PyPDF2.PdfFileReader(file)
+def multi_choice_ans_finder(pdf):
+    if not(pdf.category and pdf.subject and pdf.year and pdf.season and pdf.time_zone and pdf.paper):
+        print("Not enough info")
+        return
+
+    url = f"https://papers.gceguide.com/{pdf.category}/{pdf.subject}/{pdf.year}/{pdf.subject_code}_{pdf.season}{pdf.year[-2:]}_ms_{pdf.paper}{pdf.time_zone}.pdf"
+    filename = Path("test.pdf")
+    response = requests.get(url)
+    filename.write_bytes(response.content)
+    file = open('test.pdf', 'rb')       
+    fileReader = PyPDF2.PdfFileReader(file)
     text = ""
+    for x in range(1, fileReader.numPages):
+        pageObj = fileReader.getPage(x)
+        text += pageObj.extractText()
+        
+    file.close()
+    os.remove("test.pdf")
+    
     ANS = "ABCD"
     answers = {}
-    for x in range(1,pdf.numPages):
-        pageObj = pdf.getPage(x)
-        text += pageObj.extractText()
-    file.close()
-    
-    while not (text[0] == "1" and text[2] in ANS):
-        text = text[1:]
+    question_num = "1"
+    blacklist = ["\n", "Cambridge", "International", "AS", pdf.subject_code]
+    for x in blacklist:
+        text = text.replace(x, "")
 
-    answers[1] = text[2]
-    text = text[5:]
-    question_num = "2"
-    while text[len(question_num)+1] in ANS:
-        answers[int(text[:len(question_num)])] = text[len(question_num)+1]
-        text = text[len(question_num)+4:]
+    if int(pdf.year) < 2017:
+        old = True
+    else:
+        old = False
+    while len(text) > 4:
+        indexs = []
+        for x in ANS:
+            if old:
+                index = text.find(" "+question_num+" "+x)
+                if index > -1:
+                    index += 1
+                indexs.append(index)
+            else: indexs.append(text.find(question_num+" "+x))
+        
+        while -1 in indexs:
+            indexs.remove(-1)
+        indexs.sort()
+        if indexs:
+            index = indexs[0]
+        else:
+            break
+        
+        answers[int(text[index:index+len(question_num)])] = text[index+len(question_num)+1]
+
         question_num = str(int(question_num)+1)
-    
-    while not (text[:2] == question_num and text[3] in ANS):
-        text = text[1:]
+        if not old:
+            text = text[index:]
 
-    while len(text) > 2 and text[len(question_num)+1] in ANS:
-        answers[int(text[:len(question_num)])] = text[len(question_num)+1]
-        text = text[len(question_num)+4:]
-        question_num = str(int(question_num)+1)
-
-    print(answers)
-    for x in range(1, 41):
-        print(x, answers[x])
+    return answers
 
 def scrape_paper(self):
     filename = Path('test.pdf')
